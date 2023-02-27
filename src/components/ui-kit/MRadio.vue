f36c5457c9f765fe18754a6910594f9e3c3ff904
MaxymZakharkiv <mzaharkiv77@gmail.com>
2023-02-27 14:13:39 +0200

    add m-radio v1.0

---
+++ src/components/ui-kit/MRadio.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <input
+      type="radio"
+      :class="classes"
+      :id="id"
+      :checked="checked"
+      :value="value"
+      :name="name"
+      :disabled="disabled"
+      @input="changeChecked"
+    />
+    <label :class="{ 'disable-m-radio': disabled }" :for="id">{{
+      label
+    }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MRadio',
+  props: {
+    id: {
+      type: String,
+      default: '',
+    },
+    label: {
+      type: String,
+      required: false,
+    },
+    checked: {
+      type: Boolean,
+      required: true,
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  computed: {
+    classes() {
+      return {
+        'disable-m-radio': this.disabled,
+      }
+    },
+  },
+  methods: {
+    changeChecked(event) {
+      this.$emit('update:checked', event.target.checked)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.disable-m-radio {
+  cursor: not-allowed;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,13 +6,15 @@ import store from './store'
 import MInput from '@/components/ui-kit/MInput'
 import MButton from '@/components/ui-kit/MButton'
 import MCheckbox from '@/components/ui-kit/Checkbox/MCheckbox'
-// import MCheckboxGroup from '@/components/ui-kit/Checkbox/MCheckboxGroup'
+import MCheckboxGroup from '@/components/ui-kit/Checkbox/MCheckboxGroup'
+import MRadio from '@/components/ui-kit/MRadio'
 
 const app = createApp(App)
 
 app.component('MInput', MInput)
 app.component('MButton', MButton)
 app.component('MCheckbox', MCheckbox)
-// app.component('MCheckboxGroup', MCheckboxGroup)
+app.component('MCheckboxGroup', MCheckboxGroup)
+app.component('MRadio', MRadio)
 
 app.use(store).use(router).mount('#app')
